9c072f4d2b84b9b535dfdf9ac1227d5548742a6e
Ivaylo Valkov <2064491V@student.gla.ac.uk>
2018-04-21 16:52:47 +0100

    Added pseudocode for alg for symmetrically drawing planar graphs

---
+++ triconnected.py
@@ -0,0 +1,7 @@
+def triconnected():
+    #call Fontet's algorithm
+    #choose and orbit of min size
+    #perform star triangulation
+    #transform graph to get an outside face with >3 edges
+    #apply Fontet's algorithm again
+    #obtain generators
